5957e13bcc43c6b4b2261f37041c34d8eb39636f
Hilda Ng <hng042899@yahoo.com>
2015-06-24 17:12:45 -0700

    Create BlocBrowser

--- BLCWebBrowserViewController.m
+++ BLCWebBrowserViewController.m
@@ -13,10 +13,16 @@
 
 @property (nonatomic, strong) UIWebView *webview;      //Add UIWebView as private property
 @property (nonatomic, strong) UITextField *textField;  //Add UITextField
+@property (nonatomic, strong) UIButton *backButton;    //Add UIButton, backbutton
+@property (nonatomic, strong) UIButton *forwardButton; //Add UIButton, forwardbutton
+@property (nonatomic, strong) UIButton *stopButton;    //Add UIButton, stopbutton
+@property (nonatomic, strong) UIButton *reloadButton;  //Add UIButton, reloadbutton
 
 @end
 
 @implementation BLCWebBrowserViewController
+
+#pragma mark - UIViewController
 
 - (void)loadView {
     UIView *mainView = [UIView new];
@@ -33,7 +39,44 @@
     self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/225.0f alpha:1];
     self.textField.delegate = self;
+    
+    //add buttons
+    self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.backButton setEnabled:NO];
+                       
+    self.forwardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.forwardButton setEnabled:NO];
+    
+    self.stopButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.stopButton setEnabled:NO];
+    
+    self.reloadButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.reloadButton setEnabled:NO];
 
+    [self.backButton setTitle:NSLocalizedString(@"Back", @"Back comnmand") forState:UIControlStateNormal];
+    [self.backButton addTarget:self.webview action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward comnmand") forState:UIControlStateNormal];
+    [self.forwardButton addTarget:self.webview action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop comnmand") forState:UIControlStateNormal];
+    [self.stopButton addTarget:self.webview action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.reloadButton setTitle:NSLocalizedString(@"Refresh", @"Reload comnmand") forState:UIControlStateNormal];
+    [self.reloadButton addTarget:self.webview action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+
+    [mainView addSubview:self.webview];
+    [mainView addSubview:self.textField];
+    [mainView addSubview:self.backButton];
+    [mainView addSubview:self.forwardButton];
+    [mainView addSubview:self.stopButton];
+    [mainView addSubview:self.reloadButton];
+    
+    
+    
+    
+    
+    
     //request URL to load in web view: next(remove hardcore URL)
     
     /*  NSString *urlString = @"http://wikipedia.org";
@@ -43,7 +86,10 @@
     */
     
     [mainView addSubview:self.webview];     //add subView to mainView
+    [mainView addSubview:self.textField];
     self.view = mainView;
+    
+    
 }
 
 - (void)viewDidLoad {
@@ -71,6 +117,8 @@
     self.webview.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
 }
 
+#pragma mark - UITextFieldDelegate
+
 //to make UIWebView load URL we enter into the text field
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
@@ -78,6 +126,11 @@
     NSString *URLString = textField.text;
     
     NSURL *URL = [NSURL URLWithString:URLString];
+    
+    //The user didn't type http: or https:
+    if (!URL.scheme) {
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
     
     if (URL) {
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
@@ -87,13 +140,17 @@
     return NO;
 }
 
+#pragma UIWebViewDelegate
 
+//will call if web page fails to load
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *) error {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                    message:[error localizedDescription]
+                                                    delegate:nil
+                                                    cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                                    otherButtonTitles:nil];
+    [alert show];
 
-
-
-
-
-
-
+}
 
 @end
