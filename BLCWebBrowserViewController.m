dfd822129e79972261793a551c7746968e1ae608
Hilda Ng <hng042899@yahoo.com>
2015-07-15 17:04:44 -0700

    Functional web browser2

--- BLCWebBrowserViewController.m
+++ BLCWebBrowserViewController.m
@@ -40,7 +40,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Website URL OR SEARCH", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/225.0f alpha:1];
     self.textField.delegate = self;
     
